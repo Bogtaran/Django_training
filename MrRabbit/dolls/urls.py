0e26b89615bbcf9aa7cc6a5da2d949b87d98052d
Bogdan <bogtaran87@yandex.ru>
2024-11-26 10:57:34 +0600

    Перенесли все маршруты dolls в dolls.urls. В views создали функцию представления categories.

--- MrRabbit/dolls/urls.py
+++ MrRabbit/dolls/urls.py
@@ -1,6 +1,9 @@
-def main():
-    pass
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('cats/', views.categories),
+]
 
 
-if __name__ == '__main__':
-    main()
--- MrRabbit/dolls/views.py
+++ MrRabbit/dolls/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Страница приложения dolls')
+
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")
